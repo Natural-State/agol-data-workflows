ff179cc25a6d9778a9bebed2a9acd9de4de72da7
Dominic Henry <dominichenry@gmail.com>
2023-10-19 11:53:15 -0300

    Updated to LLBN and metadata path

--- agol_dirs.py
+++ agol_dirs.py
@@ -1,9 +1,9 @@
 import os
-import pandas as pd
 
 proj_dir = r"C:\Users\DominicH\OneDrive - Natural State\ArcPro projects\agol-data-workflows"
-metadata_dir = r"C:\Users\DominicH\OneDrive - Natural State\ArcPro projects\agol-data-workflows\agol_layers_metadata.xlsx"
+metadata_dir = r"C:\Users\DominicH\OneDrive - Natural State\Shared Documents - Data Science Division\General\AGOL\agol_layers_metadata_10-2023.xlsx"
 gee_dir = r"H:\My Drive\GEE_exports"
 api_input_dir = os.path.join(proj_dir, r"api_input")
 move_dir = os.path.join(gee_dir, "sent_to_gdb")
-clip_boundary_name = "MKR"
+clip_boundary_name = "LLBN"
+
